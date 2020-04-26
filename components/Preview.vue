3aca449804d10dd4a93bb46f693ddf21335ae383
ccj7373 <ccj7373@126.com>
2020-04-26 20:37:55 +0800

    up

--- components/Preview.vue
+++ components/Preview.vue
@@ -1,36 +1,84 @@
 <template>
-  <main class="simulator">
-    <header class="simulator__header-bar">
-      <div class="bar__operator">中国移动</div>
-      <div class="bar__time">19:01:30</div>
-      <div class="bar__battery">100%</div>
-    </header>
-    <section class="simulator__url">
-      <input
-        type="text"
-        :value="src"
-      >
-    </section>
-    <div class="doc-simulato">
-      <iframe
-        :src="src"
-        scrolling="auto"
-        frameborder="0"
-        height="715"
-        width="420"
-        class="simulator__preview"
-      ></iframe>
-    </div>
-  </main>
+  <div>
+    <a-button type="primary" @click="showModal">选项</a-button>
+    <a-modal
+      title="选项"
+      :visible="visible"
+      footer=""
+      :width="700"
+      @cancel="handleCancel"
+    >
+      <v-jsoneditor
+        v-model="json"
+        :options="options"
+        :plus="false"
+        :height="'400px'"
+      />
+      <a-button type="primary" @click="optionSubmit">设置</a-button>
+      <a-button @click="handleCancel">关闭</a-button>
+    </a-modal>
+    <main class="simulator" @click="showModal">
+      <header class="simulator__header-bar">
+        <div class="bar__operator">中国移动</div>
+        <div class="bar__time">19:01:30</div>
+        <div class="bar__battery">100%</div>
+      </header>
+      <section class="simulator__url">
+        <input type="text" :value="src" />
+      </section>
+      <div class="doc-simulato">
+        <iframe
+          :src="src"
+          scrolling="auto"
+          frameborder="0"
+          height="715"
+          width="420"
+          class="simulator__preview"
+        ></iframe>
+      </div>
+    </main>
+  </div>
 </template>
 <script>
+import VJsoneditor from "v-jsoneditor/src/index";
+
 export default {
   name: "Preview",
   props: {
     src: {
-      type: String
+      type: String,
+    },
+  },
+  components: {
+    VJsoneditor,
+  },
+  data: () => ({
+    json: {},
+    options: {},
+    visible: false,
+  }),
+  mounted() {
+    if (this.$frontmatter && this.$frontmatter.defaultData) {
+      this.$set(this, "json", this.$frontmatter.defaultData);
     }
-  }
+  },
+  methods: {
+    handleCancel() {
+      this.visible = false;
+    },
+    showModal() {
+      this.visible = true;
+    },
+    optionSubmit() {
+      document
+        .getElementsByClassName("simulator__preview")[0]
+        .contentWindow.postMessage({ type: "setData", value: this.json }, "*");
+      this.visible = false;
+    },
+    onJsonChange(value) {
+      console.log("value:", value);
+    },
+  },
 };
 </script>
 <style lang="less" scoped>
